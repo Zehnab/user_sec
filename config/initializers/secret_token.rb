0ceb9b880611b8270db5ff6015f2800034eb020e
Zehnab <zyafzal@dundee.ac.uk>
2013-05-23 19:35:38 +0100

    user authentications

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UserSec::Application.config.secret_token = 'ba72668d21d0ba7a52d873452a41d1e9a72c4595579ca4638c509dc76036af388286b7f8de8c5b32290c65d801ae9dbae5286521780469081a968bdf51d2952b'
